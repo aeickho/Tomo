9842e87a163fac5cac31bc0c2915a304dff40fb5
Wilston Oreo <wilston@khlavkalash.(none)>
2012-06-08 17:41:22 +0200

    Added comments for mesh.hpp

--- include/tomo/Mesh.hpp
+++ include/tomo/Mesh.hpp
@@ -13,9 +13,12 @@ namespace tomo
   {
     public:
       TriangleKDTree();
+      
+      // Traverses kd-tree along a ray recursively
       float recKDTreeTraverse(Ray& ray, Node* node, float tnear, float tfar, bool& found) const;
 
     private:
+      // Method to define how KDTree is constructed 
       void divideNode(Node* node, const BoundingBox& box, int depth);
   };
 
@@ -23,20 +26,29 @@ namespace tomo
   {
       void read(string filename);
 
+      // Test if mesh intersects ray.
+      // @detail  Uses TriangleKDTree for fast traversal
       bool intersect(Ray& ray) const;
+
+      // Draws Mesh with OpenGL
+      // TODO: Draw with display list
       void draw(Color color = Color()) const;
 
+      // Split a mesh into halves along a split plane
       std::pair<Mesh,Mesh> split(const Plane& plane);
-      
+   
       TexCoords texCoords(const Ray& ray) const { return TexCoords(); }
       Vec3f normal(const Ray& ray) const { return Vec3f(); }
-
-      
+   
       void displayNormals();
     protected:
+
+      // Iterate over triangles and determine maximum  
       void calcBoundingBox();
     private:
 
+      // Splits are triangle with splitting plane
+      // Adds triangles behind plane to behind and triangles in front of plane to front 
       void splitTriangle(const Triangle& tri, const Plane& plane, Mesh& behind, Mesh& front);
       TriangleKDTree kdTree;
 
