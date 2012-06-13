74d4edf276230ff9e9a9e4804ff465da267c2549
patgithub <p.hoffmann@berlin.de>
2012-06-13 04:15:14 +0200

    added animation concept

---
+++ include/tomo/anim.hpp
@@ -0,0 +1,122 @@
+#include <QtGui/QGLWidget>
+#include <ptr_vector>
+
+namespace tomo {
+  
+  /// animation processing
+  namespace anim {
+    /// define default data type
+    typedef GLDouble DEFAULT_TYPE;
+    /// define default time type
+    typedef unsigned long DEFAULT_TIME;
+
+    /** basic acceleration processor with infinite acceleraton
+     * @tparam TIME time type to use
+     */
+    template<class TIME=DEFAULT_TIME> 
+    struct Acc {
+      /** define local time type */
+      typedef TIME Time;
+      /** create acceleration processor */
+      Acc( Time _start, Time _duration ) 
+      : start_(_start)
+      , duration_(_duration) {
+      }
+      Time elapsed(Time _time) { 
+        if( finished(_time) )
+          return duration_;
+        else
+          return _time - start_; 
+      }
+      bool finished(Time _time) const {
+        return _time >= start_ + duration_; 
+      }
+      Time duration() const { return duration_; }
+    protected:
+      Time  start_;
+      Time  duration_;
+    };
+
+    template<class TIME=DEFAULT_TIME> 
+    struct Linear : public Acc<TIME> {
+      typedef TIME Time;
+      typedef Acc<Time> Base;
+      Linear( Time _start, Time _duration ) 
+      : Base(_start, _duration) { 
+      }
+      template<class TYPE> 
+      TYPE progress( Time _time ) const { 
+        return elapsed(_time) / Base::duration_;
+      }
+    };
+    
+    template<class TYPE=DEFAULT_TYPE> 
+    struct Objective {
+      typedef TYPE Type;
+      virtual void begin(Type _progress ) = 0;
+      virtual void end() = 0;
+    };
+      
+    template<class TYPE=DEFAULT_TYPE> 
+    struct MatrixTransformation : Objective<TYPE> {
+      typedef TYPE Type;
+      typedef Objective<Type> Base;
+      virtual void begin(Type _progress ) {
+        glPushMatrix();
+        transform(_progress);
+      }
+      virtual void end() {
+        glPopMatrix();
+      }
+    protected:
+      virtual void transform( Type _progress ) = 0;
+    };
+
+     template<class TYPE=DEFAULT_TYPE> 
+    struct Rotation : MatrixTransformation<TYPE> {
+      typedef TYPE Type;
+      typedef MatrixTransformation<Type> Base;
+      Rotation(Type _angle, Type _x, Type _y, Type _z) 
+      : angle_(_angle) 
+      , x_(_x), y_(_y), z_(_z) {
+      }
+      virtual void transform( Type _progress ) {
+        glRotate(_progress,x_,y_,z_);
+      }
+    protected:
+      Type angle_, x_, y_, z_;
+    };
+
+    template<class TYPE=DEFAULT_TYPE>
+    struct Objectives : Objective<TYPE> {
+      typedef TYPE Type;
+      typedef std::ptr_vector<Objective<TYPE> Container;
+      typedef Objective<Type> Base;
+      virtual void begin(const Type _progress) {
+        BOOST_FOREACH( Container::reference r, transformations_ )
+          r.begin(_progress);
+      }
+      virtual void end() {
+        BOOST_FOREACH( Container::reference r, transformations_ )
+          r.end();
+      }
+    protected:
+      Container transformations_;
+    };
+  }
+  typedef anim::Linear<> Linear;
+  typedef anim::Rotation<> Rotation;
+
+  namespace examples {
+    void f() {
+      Linear  acc(0.0,100.0);
+      Rotation rot(180.0, 0.0, 0.0, 1.0);
+      for( anim::DEFAULT_TIME i=0.0; i<acc.duration(); i += 1.0 ) {
+        anim::DEFAULT_TYPE progress=acc.progress<anim::DEFAULT_TIME>(i);
+        rot.begin(progress);
+        /// draw oject
+        rot.end();
+      }
+    }
+  }
+ }
