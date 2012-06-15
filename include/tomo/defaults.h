66644b4ee972446a7e531f8824d1f3eaecbe38e1
patgithub <p.hoffmann@berlin.de>
2012-06-15 13:54:44 +0200

    added default types header

---
+++ include/tomo/defaults.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace tomo {
+  namespace defaults {
+    typedef double CoordType;
+    typedef unsigned long TimeType;
+  }
+  typedef defaults::CoordType DefCoordType;
+  typedef defaults::TimeType  DefTimeType;
+}}
