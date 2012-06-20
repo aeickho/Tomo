f6588a149c1747c2c296e33e0ac3589559732b76
patgithub <p.hoffmann@berlin.de>
2012-06-20 03:38:46 +0200

    separated Camera into IMouse interface, Tracker, SphericalTracker and SphericalCamera

--- include/tomo/Camera.hpp
+++ include/tomo/Camera.hpp
@@ -1,32 +1,20 @@
 #pragma once
-#include "tomo/Ray.hpp"
-#include <math.h>
-#include <algorithm>
+#include "tomo/SphericalTracker.hpp"
 
 namespace tomo
 {
-  /** @brief camera base class template
-   * @tparam COORD coordinate skalar type
-   */
-  template<class COORD> struct Camera
+  template<int DIMENSIONS, class COORD, class ANGLE=COORD> struct SphericalCamera :
+      SphericalTracker<DIMENSIONS,COORD,ANGLE>
   {
-  };
-
-  /** @brief spherical camera
-   * @tparam COORD coordinate skalar type
-   */
-  template<class COORD> struct SphericCamera : Camera<COORD>
-  {
+    typedef SphericalTracker<DIMENSIONS,COORD> Base;
     /// coordinate type
     typedef COORD Coord;
     /// angle type
-    typedef COORD Angle;
+    typedef ANGLE Angle;
     /// 3D point
-    typedef tomo::Point<3,COORD> Point;
-
-    /** @brief default constructor
-     */
-    SphericCamera() : center_(0,0,0), latitude_(0), longitude_(0), distance_(0), up_(0,1,0) { }
+    typedef tomo::Point<DIMENSIONS,COORD> Point;
+    /// dimensions
+    static const int dimensions_ = DIMENSIONS;
 
     /** @brief setup camera parameters
      * @param _longitude initial longitude
@@ -34,60 +22,16 @@ namespace tomo
      * @param _near minimum visible distance to object
      * @param _far maximum visible distance from object
      * @param _distance initial distance from object
+     * @param _up camera orientation
      */
-    void setup( Angle _longitude, Angle _latitude, Coord _near, Coord _far, Coord _distance )
+    void setup( Angle _longitude, Angle _latitude, Coord _near, Coord _far, Coord _distance, const Point& _up )
     {
-      longitude_ = _longitude;
-      latitude_ = _latitude;
-      near_ = _near;
-      far_ = _far;
-      distance_ = _distance;
+      Base::setup(_longitude, _latitude, _near, _far, _distance);
+      up_ = _up;
+      Base::update();
     }
 
-    /** @brief relatively move the camera on the sphere
-     * @param _relativeLongitude relative longitude move in degrees
-     * @param _relativeLatitude relative latitude move in degrees
-     */
-    void move( Angle _relativeLongitude, Angle _relativeLatitude )
-    {
-      longitude_ += _relativeLongitude;
-      latitude_ = std::max(std::min(latitude_ + _relativeLatitude,(Coord)90),(Coord)-90);
-    }
-
-    /** @brief change distance between camera and center
-     * @param _relativeDistance relative distance move
-     * @param _border let gap between near and far distance when zooming
-     */
-    void zoom( Coord _relativeDistance, Coord _border=0 )
-    {
-      distance_ = std::min(std::max(distance_ + _relativeDistance, near_ + _border),far_ - _border);
-    }
-
-    /** @brief update eye property based on spherical coordinates
-     */
-    void update()
-    {
-      /// @todo optimize: move the division to getter/setter of meridian and latiude?!
-      eye_.x( center_.x() + distance_ * cos( latitude_ / 180.0 * M_PI ) * cos( longitude_ / 180.0 * M_PI ) );
-      eye_.y( center_.y() + distance_ * sin( latitude_ / 180.0 * M_PI ) );
-      eye_.z( center_.z() + distance_ * cos( latitude_ / 180.0 * M_PI ) * sin( longitude_ / 180.0 * M_PI ) );
-    }
-
-    /// point of interest
-    TBD_PROPERTY(Point,center);
-    /// latitude of the camera postition on a sphere arround center
-    TBD_PROPERTY(Angle,latitude);
-    /// longitude/meridian of the camera position on a sphere arround center
-    TBD_PROPERTY(Angle,longitude);
-    /// distance of the camera from center
-    TBD_PROPERTY(Coord,distance);
     /// camera orientation
     TBD_PROPERTY(Point,up);
-    /// near z-clipping / maximum zoom
-    TBD_PROPERTY(Coord,near);
-    /// far z-clipping / minimum zoom
-    TBD_PROPERTY(Coord,far);
-    /// camera position
-    TBD_PROPERTY(Point,eye);
   };
 }
--- src/cg2ex1/glwidget.h
+++ src/cg2ex1/glwidget.h
@@ -11,46 +11,46 @@
 class GLWidget : public QGLWidget
 {
   Q_OBJECT
-  public:
-    typedef tomo::SphericCamera<GLfloat> Camera;
-    typedef tomo::Color<4,GLfloat> Color;
-    typedef tomo::Light<Color,GLfloat,4> Light;
-    typedef tomo::Point<4,GLfloat> Point;
+public:
+  typedef tomo::SphericalCamera<4,float> Camera;
+  typedef tomo::Color<4,float> Color;
+  typedef tomo::Light<Color,GLfloat,4> Light;
+  typedef tomo::Point<4,float> Point;
 
-    explicit GLWidget(QWidget *parent = 0);
-    enum SelectionMode { SELECT_KNEAREST, SELECT_RADIUS };
-  protected:
-    virtual void mouseMoveEvent(QMouseEvent *event);
-    virtual void mousePressEvent(QMouseEvent *event);
-    virtual void wheelEvent(QWheelEvent *event);
-    virtual void initializeGL();
-    virtual void resizeGL(int w, int h);
-    virtual void paintGL();
-    void paintCamera();
-    void paintObjects();
-    void paintSelection();
-  public:
-    /// @todo replace with an vector<tomo::SceneObjects> 
-    tomo::Mesh mesh_;
-    /// selection parameters
-    float pointSize_;
-    float radius_;
-    int kNearest_;
-    SelectionMode selectionMode_;
-    Point selection_;
+  explicit GLWidget(QWidget *parent = 0);
+  enum SelectionMode { SELECT_KNEAREST, SELECT_RADIUS };
+protected:
+  virtual void mouseMoveEvent(QMouseEvent *event);
+  virtual void mousePressEvent(QMouseEvent *event);
+  virtual void wheelEvent(QWheelEvent *event);
+  virtual void initializeGL();
+  virtual void resizeGL(int w, int h);
+  virtual void paintGL();
+  void paintCamera();
+  void paintObjects();
+  void paintSelection();
+public:
+  /// @todo replace with an vector<tomo::SceneObjects>
+  tomo::Mesh mesh_;
+  /// @brief selection parameters
+  float pointSize_;
+  float radius_;
+  int kNearest_;
+  SelectionMode selectionMode_;
+  Point selection_;
 
-    /// last mouse position in window
-    QPoint mousePosition_;
-    /// animation timer
-    QTimer* timer_;
-    /// camera position/view
-    Camera camera_;
-    /// light parameters
-    Light light_;
-    /// perspective
-    public slots:
-      /// triggered by the animation timer
-      virtual void tick();
+  /// last mouse position in window
+  QPoint mousePosition_;
+  /// animation timer
+  QTimer* timer_;
+  /// camera position/view
+  Camera camera_;
+  /// light parameters
+  Light light_;
+  /// perspective
+public slots:
+  /// triggered by the animation timer
+  virtual void tick();
 };
 
 #endif // GLWIDGET_H
