88de5a8af5158d98edb4534ab404c3e911d79d57
patgithub <p.hoffmann@berlin.de>
2012-06-22 01:17:33 +0200

    new PolarVec

---
+++ include/tomo/PolarVec.hpp
@@ -0,0 +1,62 @@
+#ifndef _POLARVEC_HPP
+#define _POLARVEC_HPP
+
+#include "tomo/Vec.hpp"
+
+namespace tomo
+{
+  template<class COORD> struct PolarVec 
+  {
+    typedef Vec<3,COORD> Vec;
+    /// coordinate type
+    typedef COORD Coord;
+    static const int dimensions_ = 3;
+
+    PolarVec() {}
+    /** @brief create polar vector with angles in degrees
+     * @param _longitude longitude position
+     * @param _latitude latitude position
+     * @param _radius radial distance
+     */
+    PolarVec( Coord _longitude, Coord _latitude, Coord _radius ) :
+      longitude_(_longitude),
+      latitude_(_latitude),
+      radius_(_radius)
+    {
+    }
+    operator Vec() const
+    {
+//      BOOST_STATIC_ASSERT(dimensions_==3);
+      Coord _latitude = deg2rad(latitude());
+      Coord _longitude = deg2rad(longitude());
+      return Vec(
+               radius() * cos(_latitude) * cos(_longitude),
+               radius() * sin(_latitude),
+               radius() * cos(_latitude) * sin(_longitude)
+             );
+    }
+    const PolarVec& operator+=( const PolarVec& _vec )
+    {
+      longitude( longitude() + _vec.longitude() );
+      latitude( std::min<Coord>(89.99,std::max<Coord>(-89.99,latitude() + _vec.latitude())) );
+      radius( std::max<Coord>(0,radius() + _vec.radius()) );
+      return *this;
+    }
+
+    TBD_PROPERTY(Coord,longitude);
+    TBD_PROPERTY(Coord,latitude);
+    TBD_PROPERTY(Coord,radius);
+
+  protected:
+    static inline Coord deg2rad( Coord _value )
+    {
+      return _value * M_PI / 180.0;
+    }
+    static inline Coord rad2deg( Coord _value )
+    {
+      return _value * 180.0 / M_PI;
+    }
+  };
+}
+
+#endif /* _POLARVEC_HPP */
