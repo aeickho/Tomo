c02d0704f53f245f04f590bc4d2e70f524db86c5
patgithub <p.hoffmann@berlin.de>
2012-06-21 12:56:25 +0200

    code review

--- include/tomo/Slicer.hpp
+++ include/tomo/Slicer.hpp
@@ -1,9 +1,6 @@
 #include "tomo/Slice.hpp"
 #include "tomo/SceneObject.hpp"
-
 #include <tbd/config.h>
-#include <boost/ptr_container/ptr_vector.hpp>
-
 
 namespace tomo
 {
@@ -12,30 +9,30 @@ namespace tomo
 
   class Slicer : public ConfigurableObject
   {
-    public:
-      Slicer(Config* _config = NULL) : ConfigurableObject(_config) {}
+  public:
+    Slicer(Config* _config = NULL) : ConfigurableObject(_config) {}
 
-	  Slices slice(const Primitive& obj, unsigned nSlices = 0) const;
+    Slices slice(const Primitive& obj, unsigned nSlices = 0) const;
 
-  //  boost::ptr_vector<Isle> fetchIsles(const SceneObject& obj, const SplitPlane& splitPlane, IsleType = ALL);
+    //  boost::ptr_vector<Isle> fetchIsles(const SceneObject& obj, const SplitPlane& splitPlane, IsleType = ALL);
 
     TBD_PROPERTY_CFG(unsigned,nSlices,"NUMBER_OF_SLICES",10);
     TBD_PROPERTY_CFG(unsigned,resX,"RESOLUTION_X",1000);
     TBD_PROPERTY_CFG(unsigned,resY,"RESOLUTION_Y",1000);
 
-    private:
-    /*  
+  private:
+    /*
     struct RaySegments
       {
-        std::map<float,bool> segments_; 
+        std::map<float,bool> segments_;
       }
 
       std::map<float,RaySegments> inter
-    
+
       Slice generateSlice(SceneObject& obj, unsigned sliceIdx)
       {
       }
-*/
+    */
 
   };
 }
