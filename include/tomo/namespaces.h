8633e0aa94cf8a32c77a40715051e32e05eacad0
Wilston Oreo <wilston@khlavkalash.(none)>
2012-09-19 23:50:22 +0200

    Namespaces description

---
+++ include/tomo/namespaces.h
@@ -0,0 +1,40 @@
+
+namespace tomo
+{
+  /// Contains all classes and method for geometry representation
+  namespace geometry 
+  {
+    /// Contains basic classes like vectors, points and color
+    namespace base
+    {
+    }
+
+    /// Contains auxiliary structures like compounds object and kdtree for fast search
+    namespace aux
+    {
+    }
+
+    /// Contains all geometric primitive types
+    namespace prim
+    {
+    }
+  }
+
+  /// All classes to slice a Mesh
+  namespace slicing
+  {
+  }
+
+  /// All classes for visualization of a scene
+  namespace scene
+  {
+  }
+
+  /// All classes for g-code representation 
+  namespace gcode
+  {
+  }
+}
+
+
+
